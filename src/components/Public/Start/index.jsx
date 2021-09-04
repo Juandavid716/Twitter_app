bd88d62379e0b50ddce8b00b1707d521de0db201
Juan <jbojato@uninorte.edu.co>
2021-09-04 16:31:52 -0500

    styles(start): added styles to the initial view

--- src/components/Public/Start/index.jsx
+++ src/components/Public/Start/index.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
 import Metadata from "../../../metadata/Metadata";
-import TwitterLogo from "../../../assets/images/twitter_logo.png";
+import TwitterWhiteLogo from "../../../assets/images/twitter_white_logo.png";
+import TwitterBlueLogo from "../../../assets/images/twitter_blue_logo.png";
 import "./start.scss";
 
 const Start = () => {
@@ -10,11 +12,31 @@ const Start = () => {
         description="The best place to share and meet new friends"
         url=""
       />
-      <main>
-        <section class="logo">
-          <img src={TwitterLogo} alt="Twitter Logo" />
+      <main className="container flex-direction">
+        <section className="logo">
+          <img src={TwitterWhiteLogo} alt="Twitter Logo" />
         </section>
-        <section class="welcome">Hola</section>
+        <section className="welcome">
+          <div className="welcome__container">
+            <img src={TwitterBlueLogo} alt="Welcome logo" />
+            <h1 className="slogan"> Welcome to Twitter</h1>
+            <div className="buttons">
+              <Link
+                to="/login"
+                className="button center text-decoration"
+                style={{ marginBottom: "40px" }}
+              >
+                Login now
+              </Link>
+              <label className="button__label">
+                Dont have an account? <span id="join">Join free today </span>{" "}
+              </label>
+              <Link to="/signup" className="button center text-decoration">
+                Sign Up
+              </Link>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
