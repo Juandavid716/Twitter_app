8bfe1b8f1c79f413254838c011621442164e5b45
Juan <jbojato@uninorte.edu.co>
2021-09-04 19:22:38 -0500

    feat(sitemap.xml): added sitemap.xml

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-//import Nav from "./components/Nav";
 import routes from "./lib/routes";
 
 const App = () => {
   const { restricted, unrestricted } = routes;
   return (
     <BrowserRouter>
-      {/* <Nav /> */}
       <Switch>
         {restricted.map(({ path, component }) => (
           <Route exact path={path} component={component} key={path} />
